9b283472e724f59e9ecb08772160fbb928de3266
Aws Nassar <166246247+Aws-Nassar@users.noreply.github.com>
2025-07-17 19:47:34 +0300

    Delete Assignments directory

--- Assignments/Assignment 6 - Final Project OOP/models/reservable.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-
-# Interface class for reservable items
-class Reservable(ABC):
-    @abstractmethod
-    def reserve(self, user):
-        """Reserve the item for a user"""
-        pass
